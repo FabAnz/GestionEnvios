c335285bfdecfb6258b5d09c1be7515b008d9bd3
FabAnz <fabian.methil@gmail.com>
2025-04-26 12:32:41 -0300

    Indices agregados en bd

---
+++ AccesoDatos/Migrations/20250426152737_IndicesAgregados.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AccesoDatos.Migrations
+{
+    /// <inheritdoc />
+    public partial class IndicesAgregados : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Email_Valor",
+                table: "Usuarios",
+                type: "nvarchar(100)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Agencias",
+                type: "nvarchar(50)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Usuarios_Email_Valor",
+                table: "Usuarios",
+                column: "Email_Valor",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agencias_Nombre",
+                table: "Agencias",
+                column: "Nombre",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Usuarios_Email_Valor",
+                table: "Usuarios");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Agencias_Nombre",
+                table: "Agencias");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email_Valor",
+                table: "Usuarios",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nombre",
+                table: "Agencias",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)");
+        }
+    }
+}
--- CasosUso/DTOs/EnvioDTO.cs
+++ CasosUso/DTOs/EnvioDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,16 @@ namespace CasosUso.DTOs
     public class EnvioDTO
     {
         public int Id { get; set; }
+        [DisplayName("N° Tracking")]
         public int NTracking { get; set; }
         public UsuarioDTO Vendedor { get; set; }
+        [DisplayName("Cliente")]
         public string EmailCliente { get; set; }
         public int Peso { get; set; }
         public string Estado { get; set; }
+        [DisplayName("Fecha de envio")]
         public DateTime FechaEnvio { get; set; }
+        [DisplayName("Fecha de entrega")]
         public DateTime FechaEntrega { get; set; }
         public List<ComentarioDTO> Comentarios { get; set; }
         public string? Direccion { get; set; }
